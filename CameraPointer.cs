e7313026795940e959e38e39e6074fa7c7090753
Dani <ds006518@ohio.edu>
2019-10-03 21:45:25 -0400

    Commented code, added sprint system on left shift press.
    Initial Commit on this branch.

--- CameraPointer.cs
+++ CameraPointer.cs
@@ -5,25 +5,25 @@ using UnityEngine.UI;
 
 public class CameraPointer : MonoBehaviour
 {
-    public float sensitivity = 10f;
-    public float maxYAngle = 80f;
-    private Vector2 currentRotation;
+    public float sensitivity = 10f; //sensitivity of the mouse
+    public float maxYAngle = 80f; //maximum angle that the player is able to look up
+    private Vector2 currentRotation; //used to facilitate rotation calculation
 
     private void Start()
     {
-        Cursor.visible = false;
+        Cursor.visible = false; //sets the default unity cursor to be invisible(?)
     }
     void Update()
     {
 
-        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
-        currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
-        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
-        currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
-        Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
-        if (Input.GetMouseButtonDown(0))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
+        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity; //gets the x component of the movement of the mouse and stores it in the x component of currentRotation
+        currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity; //gets the y component of the movement of the mouse and stores it in the y component of currentRotation
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360); //loops currentRotation.x so it is always greater than 0 and less than 360 then sets it to the currentrotation
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle); //clamps the Y component of the camera rotation so it cannot be above or below a maxYangle.
+        Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0); //sets the camera rotation to = currentRotation
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Cursor.lockState = CursorLockMode.Locked; //i dont think this is needed anymore.
+        //}
     }
 }
--- Movement.cs
+++ Movement.cs
@@ -4,56 +4,70 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    float speed = 5.0f;
-    public float sensitivity = 10f;
-    private Vector2 currentRotation;
-    public AudioSource flamethrowerSound;
-    public ParticleSystem flameThrower;
+    float speed = 5.0f; // a multiplier added to the movement of the player.
+    float sprintModifier = 1.5f; // the modifier that leftshift adds
+    public float sensitivity = 10f; //sensitivity of the mouse
+    private float finalSprintMod = 1; //decides if the sprint mod will be applied
+    private Vector2 currentRotation; //used to facilitiate calculation of the rotation in the RotateObject section of the script
+    //public AudioSource flamethrowerSound; //depricated
+    //public ParticleSystem flameThrower; //depricated
+
     private void Start()
     {
-        flameThrower.Stop();
-    }
+        //flameThrower.Stop(); deprecated code, carryovers from Burnie 2020(?)
+    }//Start
+
     private void Update()
     {
         Move();
         RotateObject();
-        if (Input.GetMouseButtonDown(1))
-            Cursor.lockState = CursorLockMode.Locked;
-        if (Input.GetMouseButtonDown(1))
+        //if (Input.GetMouseButtonDown(1)) 
+            //Cursor.lockState = CursorLockMode.Locked; //commented out because I think an FPS like frogs should omit a feature like this
+        if (Input.GetMouseButtonDown(1)) //if lmb pressed
         {
-            flamethrowerSound.Play();
-            flameThrower.Play();
+            //flamethrowerSound.Play(); depricated
+            //flameThrower.Play(); depricated
         }
-        if(Input.GetMouseButtonUp(1))
+        if(Input.GetMouseButtonUp(1)) //if lmb unpressed
         {
-            flamethrowerSound.Stop();
-            flameThrower.Stop();
+            //flamethrowerSound.Stop();
+            //flameThrower.Stop(); another carryover from Burnie 2020 
         }
 
-    }
-    private void Move()
+    }//Update
+
+    private void Move() //seperated function for moving the player(model?)
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            finalSprintMod = sprintModifier;
+        }
+        else
+        {
+            finalSprintMod = 1;
+        }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * speed * finalSprintMod * Time.deltaTime); //translate player forward by the speed * deltatime
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * speed * finalSprintMod * Time.deltaTime); //translate player left by the speed * deltatime
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            transform.Translate(Vector3.back * speed * finalSprintMod * Time.deltaTime); //translate player forward by the speed * deltatime
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.Translate(Vector3.right * speed * finalSprintMod * Time.deltaTime); //translate player right by the speed * deltatime
         }
-    }
-    private void RotateObject()
+    }//Move
+
+    private void RotateObject() //seperated function for rotating the player(model?)
     {
-        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
-        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
-        gameObject.transform.rotation = Quaternion.Euler(0, currentRotation.x, 0);
+        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity; //sets the x component of the Vector2 'currentRotation' to be the movement of the mouse * sensitivity
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360); //loops currentRotation.x so it is always greater than 0 and less than 360 then sets it to the currentrotation
+        gameObject.transform.rotation = Quaternion.Euler(0, currentRotation.x, 0); //set the x transform of the player to be the x component of currentrotation
     }
-}
+}//Rotate Object
